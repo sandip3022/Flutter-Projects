a6c72999d58365102f6d74d137b1eb610dc844bc
Sandip <sandipanap2001@gmail.com>
2025-01-02 22:44:52 +0530

    timer added, and code refactor.

---
+++ Widet_Academy_App/widget_academy/lib/home/welcome_page.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage>{
+  @override
+  void initState() {
+
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return  const Scaffold(body: Center(child: Text('Welcome Page') ,),
+    );
+    
+    
+  }
+}
--- Widet_Academy_App/widget_academy/lib/splash.dart
+++ Widet_Academy_App/widget_academy/lib/splash.dart
@@ -1,13 +1,32 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:widget_academy/home/welcome_page.dart';
 
 class SplashPage extends StatefulWidget {
-  const SplashPage({super.key});
+  const SplashPage({super.key,});
+
+  @override
+  State<StatefulWidget> createState() => _SplashPageState();
+  }
 
   @override
   _SplashPageState createState() => _SplashPageState();
-}
+
 
 class _SplashPageState extends State<SplashPage> {
+
+   @override
+  void initState() {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const WelcomePage()),
+      );
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
